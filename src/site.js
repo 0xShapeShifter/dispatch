34d21770f0215e5971905d52231fb0212de87b0c
0xShapeShifter <113918140+0xShapeShifter@users.noreply.github.com>
2024-04-18 08:56:32 +0300

    ⚒️ Built the site

---
+++ src/site.js
@@ -0,0 +1,68 @@
+export const site = {
+    title: "Dispatch",
+    description: "— your development partner for a simple monthly fee",
+    lead: "stop wasting time on the hiring process overhead, and unreability of freelancers",
+    call_link: "cal.com/", // f.ex, or https://cron.com/
+    subscription_link: "https://stripe.com/", //f.ex, or https://www.lemonsqueezy.com/
+    subscription: {
+        price: 8000,
+        spots: 1,
+        description:
+            "Senior developer joins forces with you. Perfect for startups, smaller studios, and pressing deadlines.",
+        benefits: [
+            "1 senior software developer",
+            "1 request at a time",
+            "Unlimited requests",
+            "Ullimited reviews",
+            "5 days a week",
+            "24-72 hours delivery, often — faster",
+            "Fully async communication",
+            "Dedicated Discord server, or Slack channel",
+        ],
+    },
+
+    services: ["Websites", "Web-apps", "Desktop apps", "Native apps"],
+    about:
+        "Hi, I am Morgan, a software engineer with 20+ years of experience in writing elegant code for web, desktop, and mobile",
+    faq: [
+        {
+            question: "Can I pause or cancel my subscription?",
+            answer:
+                "Absolutely! You can pause or cancel your subscription anytime. No questions asked.",
+        },
+        {
+            question: "When can I pause my subscription?",
+            answer: "You can cancel your subscription anytime.",
+        },
+        {
+            question: "How quickly do we start?",
+            answer:
+                "We can start immediately within working hours of a working week!",
+        },
+        {
+            question: "How do we begin?",
+            answer:
+                "We usually start by having a call. When you subscribe, you'll receive an email will all the instructions. You will also find a link for an intro call. The call is probably the only synchronous interaction we will have.",
+        },
+        {
+            question: "Can I hire you if I run my own agency/studio/team?",
+            answer: "Sure, no problem. Feel free to offload some of your work to us",
+        },
+        {
+            question: "What if we find bugs or something doesn't work?",
+            answer: "You immediately reach out, and we deal with it ASAP.",
+        },
+    ],
+    benefits: [
+        "Hiring full-time is slow, costly, and it takes time to reap its benefits",
+        "Most dev studios and agencies are outsourcing themselves, meanwhile requiring to pay the brand tax",
+    ],
+    contacts: [
+        { title: "Email", href: "mailto:hi@essens.dev" },
+        { title: "GitHub", href: "https://github.com/essens-cc" },
+        {
+            title: "Mastodon",
+            href: "https://bsky.app/profile/essenscc.bsky.social",
+        },
+    ],
+};
